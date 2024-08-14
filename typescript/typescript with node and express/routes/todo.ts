37fc380c4702f38ab45b54973f686ad6593cc41c
arex123 <ad47kumar@gmail.com>
2024-08-14 23:27:14 +0530

    more optimized delete and update method

--- typescript/typescript with node and express/routes/todo.ts
+++ typescript/typescript with node and express/routes/todo.ts
@@ -3,77 +3,65 @@ import { Router } from "express";
 const router = Router()
 import { Todo } from "../models/todo";
 
-const todo:Todo[] = []
+let todo: Todo[] = []
 
-router.get('/',(req,res,next)=>{
+router.get('/', (req, res, next) => {
     res.status(200).json(todo)
 })
 
-router.post('/todo',(req,res,next)=>{
-    let newTodo:Todo={
+router.post('/todo', (req, res, next) => {
+    let newTodo: Todo = {
         id: new Date().toDateString(),
         text: req.body.text
     }
     todo.push(newTodo)
 
     res.status(201).json({
-        success:true,
+        success: true,
         newTodo
     })
 })
 
+router.delete('/todo/:id', (req, res, next) => {
+    const id = req.params.id;
 
-router.delete('/todo',(req,res,next)=>{
-    let id = req.body.id
-    let flag=0
-    for(let i=0;i<todo.length;i++){
-        if(todo[i].id==id){
-            delete todo[i]
-            flag=1
-            break
-        }
-        
+    const index = todo.findIndex(item => item.id == id);
+
+    if (index !== -1) {
+        todo.splice(index, 1);
+        return res.status(200).json({ success: true });
+    } else {
+        return res.status(404).json({
+            success: false,
+            message: "Item not found"
+        });
     }
+});
 
-    if(flag==1){
-
-        res.status(200).json({
-            success:true
-        })
-    }else{
-        res.status(404).json({
-            success:false,
-            message:"Item not found"
-        })
-
-    }
-})
-
-router.put('/todo/:id',(req,res,next)=>{
+router.put('/todo/:id', (req, res, next) => {
     let id = req.params.id
     let newText = req.body.newText
-    let flag=0
-    for(let i=0;i<todo.length;i++){
-        if(todo[i].id==id){
-            todo[i].text = newText
-            flag=1
-            break
+
+    let todoIdx = todo.findIndex((item) => item.id == id)
+    if (todoIdx >= 0) {
+        todo[todoIdx] = {
+            id:id,
+            text:newText
         }
-        
+
+        return res.status(200).json({
+            success:true,
+            message:"successfuly updated",
+            todo
+        })
     }
 
-    if(flag==1){
+    res.status(404).json({
+        success: false,
+        message: "Item not found"
+    })
 
-        res.status(200).json({
-            success:true
-        })
-    }else{
-        res.status(404).json({
-            success:false,
-            message:"Item not found"
-        })
 
-    }
 })
 
 export default router
